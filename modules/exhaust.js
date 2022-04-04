2bc72d897c244a70a1ff3965631ff64c4ffaa6e1
Ubuntu <maddie.madlad@gmail.com>
2022-04-04 17:58:40 +0200

    exhaust driver up and ready

---
+++ modules/exhaust.js
@@ -0,0 +1,21 @@
+import child_process from "child_process"
+import {shell_exec} from "./utils.js"
+
+const ops = {
+on: "pigs w 21 0",
+off: "pigs w 21 1",
+state: "pigs r 21"
+}
+
+
+export function switchOn(){
+  shell_exec(ops.on)
+}
+export function switchOff(){
+  shell_exec(ops.off)
+}
+export function getState(callback){
+  shell_exec(ops.state, (res)=>{
+    callback(res)
+  })
+}
