28c63afc1c804e3af16a83a1c70049bb0561a4e5
Ubuntu <maddie.madlad@gmail.com>
2022-04-04 18:16:11 +0200

    moved growlight getschedule to growlightjs

--- modules/system.js
+++ modules/system.js
@@ -4,17 +4,9 @@ import {shell_exec, queryDatabase as query} from "./utils.js"
 const ops={
   space_data: "df -h|grep /dev/root|awk '{print $2\" \"$3\" \"$4\" \"$5}'",
   cputemp: "sensors cpu_thermal-virtual-0|grep temp1|awk '{print $2}'|tr -d '+'",
-  db_size: "SELECT table_schema \\\"growbox_db\\\",ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) \\\"DB Size in MB\\\" FROM information_schema.tables GROUP BY table_schema;",
-  crontab: "crontab -l"
-
+  db_size: "SELECT table_schema \\\"growbox_db\\\",ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) \\\"DB Size in MB\\\" FROM information_schema.tables GROUP BY table_schema;"
 }
 
-
-export function getSchedules(callback){
-  shell_exec(ops.crontab,(res)=>{
-    callback(res)
-  })
-}
 
 export function getSpaceData(callback){
   shell_exec(ops.space_data,(res)=>{
