92c850295f77331a2369607805fb853704944a8e
Ubuntu <maddie.madlad@gmail.com>
2022-04-04 19:22:02 +0200

    hotfix

--- modules/sensors.js
+++ modules/sensors.js
@@ -1,6 +1,6 @@
 import {queryDatabase as query} from "./utils.js"
 
-export const queries = {
+const ops = {
   dht1_single: "SELECT id,temperature,humidity,time_stamp FROM DHT ORDER BY id DESC LIMIT 1;",
   dht2_single: "SELECT id,temperature,humidity,time_stamp FROM DHT2 ORDER BY id DESC LIMIT 1;",
   dst_single: "SELECT id,temperature,time_stamp FROM DST ORDER BY id DESC LIMIT 1;"
