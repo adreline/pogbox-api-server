7c1b4a436ee1a7ac74deca9494140aa549950d37
Ubuntu <maddie.madlad@gmail.com>
2022-04-04 18:09:00 +0200

    sensors driver is up and ready

---
+++ modules/sensors.js
@@ -0,0 +1,24 @@
+import {queryDataBase as query} from "./utils.js"
+
+export const queries = {
+  dht1_single: "SELECT id,temperature,humidity,time_stamp FROM DHT ORDER BY id DESC LIMIT 1;",
+  dht2_single: "SELECT id,temperature,humidity,time_stamp FROM DHT2 ORDER BY id DESC LIMIT 1;",
+  dst_single: "SELECT id,temperature,time_stamp FROM DST ORDER BY id DESC LIMIT 1;"
+}
+
+
+export function readDht1(callback){
+  query(ops.dht1_single,(res)=>{
+    callback(res)
+  })
+}
+export function readDht2(callback){
+  query(ops.dht2_single,(res)=>{
+    callback(res)
+  })
+}
+export function readDst(callback){
+  query(ops.dst_single,(res)=>{
+    callback(res)
+  })
+}
